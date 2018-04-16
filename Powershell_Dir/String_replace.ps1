9c33e639eb413dd23280ae2b926cb63a61c7d3e4
mraja1982 <38427194+mraja1982@users.noreply.github.com>
2018-04-16 17:20:25 +0530

    GSC Test
    GSC Test

---
+++ Powershell_Dir/String_replace.ps1
@@ -0,0 +1,12 @@
+﻿$inStr = Read-Host "Input String "
+$inPos = Read-Host "Enter Position of String "
+$chArray = @()
+$chArray = $inStr.ToCharArray()
+Write-Host "String replace: "
+for($i=0;$i -lt $chArray.Length;$i++)
+{
+    if(!($i -eq $inPos-1))
+    {
+        Write-Host $chArray[$i] -NoNewline
+    }
+}
---
+++ Powershell_Dir/string_to_number.ps1
@@ -0,0 +1,37 @@
+﻿$inStr = Read-Host "Input String: "
+$chArray = @()
+$chArray = $inStr.ToCharArray()
+Write-Host "String after number conversion: "
+for($i=0;$i -lt $chArray.Length;$i++)
+{
+    switch($chArray[$i])
+    {
+        "a" {write-host "1" -NoNewline;break}
+        "b" {write-host "2" -NoNewline;break}
+        "c" {write-host "3" -NoNewline;break}
+        "d" {write-host "4" -NoNewline;break}
+        "e" {write-host "5" -NoNewline;break}
+        "f" {write-host "6" -NoNewline;break}
+        "g" {write-host "7" -NoNewline;break}
+        "h" {write-host "8" -NoNewline;break}
+        "i" {write-host "9" -NoNewline;break}
+        "j" {write-host "10" -NoNewline;break}
+        "k" {write-host "11" -NoNewline;break}
+        "l" {write-host "12" -NoNewline;break}
+        "m" {write-host "13" -NoNewline;break}
+        "n" {write-host "14" -NoNewline;break}
+        "o" {write-host "15" -NoNewline;break}
+        "p" {write-host "16" -NoNewline;break}
+        "q" {write-host "17" -NoNewline;break}
+        "r" {write-host "18" -NoNewline;break}
+        "s" {write-host "19" -NoNewline;break}
+        "t" {write-host "20" -NoNewline;break}
+        "u" {write-host "21" -NoNewline;break}
+        "v" {write-host "22" -NoNewline;break}
+        "w" {write-host "23" -NoNewline;break}
+        "x" {write-host "24" -NoNewline;break}
+        "y" {write-host "25" -NoNewline;break}
+        "z" {write-host "26" -NoNewline;break}
+        default{Write-Host "Not Valid";break}
+    }
+}
